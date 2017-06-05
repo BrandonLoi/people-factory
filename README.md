1ea376f50951a65c61b5ba04547520b43c22ced7
Brandon Loi <brandon.loi@gmail.com>
2017-06-05 15:28:05 -0400

    Add button to increase size of text

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # People Factory
+
+### (completed) Super Mega Bonus Credit
+
+* Add an empty paragraph to the page.
+* Use the form values to do something with that paragraph.
+
+### Super Mega Bonnus Credit Hyper Fighting
+
+* Change the appearance of the paragraph (think CSS) based on a value from the form.
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const personForm = document.querySelector('form#personForm')
+const size = document.querySelector('form#size')
+
 
 function HandleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
     const age = f.age.value
-    document.querySelector('h1').textContent = name + ' ' + age;
+    document.querySelector('h1').textContent = name + ' ' + age
+    document.getElementById('blank').textContent += name + '-' + age + ' \n'
+}
+function fontSize(ev) {
+    ev.preventDefault()
+    document.getElementById('sizeChange').style.fontSize = "20px"
+
 }
 
+
 personForm.addEventListener('submit', HandleSubmit)
+size.addEventListener('submit', fontSize)
