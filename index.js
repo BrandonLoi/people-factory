20df5b66c1f43aaf1da08ad4c207e4e400d01dff
Brandon Loi <brandon.loi@gmail.com>
2017-06-05 16:07:07 -0400

    Clean and reorganize code

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const size = document.querySelector('form#size')
 
 
 function HandleSubmit(ev) {
-    ev.preventDefault()
-    const f = ev.target
-    const name = f.personName.value
-    const age = f.age.value
-    document.querySelector('h1').textContent = name + ' ' + age
-    document.getElementById('blank').textContent += name + '-' + age + ' \n'
+  ev.preventDefault()
+  const f = ev.target
+  const name = f.personName.value
+  const age = f.age.value
+  document.querySelector('h1').textContent = name + ' ' + age
+  document.getElementById('blank').textContent += name + '-' + age + ' \n'
 }
+
 function fontSize(ev) {
-    ev.preventDefault()
-    document.getElementById('sizeChange').style.fontSize = "20px"
+  ev.preventDefault()
+  document.getElementById('sizeChange').style.fontSize = "20px"
 
 }
 
