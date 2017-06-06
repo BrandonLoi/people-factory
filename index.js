fcb9c11e5ccc46bafa2ada456203f07d077c8dd6
Brandon Loi <brandon.loi@gmail.com>
2017-06-06 10:21:24 -0400

    Add color box

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function HandleSubmit(ev) {
   const details = document.querySelector('#details')
   const favoriteColor = f.favoriteColor.value;
   const age = f.age.value
+  const colorDiv = `
+  <div style="background-color: ${favoriteColor}; width: 100px; height:50px; "></div>`
   //document.querySelector('h1').textContent = name + ' ' + age
   //document.getElementById('blank').textContent += name + '-' + age + ' \n'
   //details.innerHTML = `<em>${name}</em>`;
@@ -16,11 +18,11 @@ function HandleSubmit(ev) {
   // em.textContent = name;
   // details.appendChild(em
   details.innerHTML = `
-  <ul>
-    <li>${name}</li>
-    <li>${favoriteColor}</li>
-    <li>${age}</li>
-  </ul>
+    <ul>
+      <li>${name}</li>
+      <li>${colorDiv}</li>
+      <li>${age}</li>
+    </ul>
   `
 }
 
