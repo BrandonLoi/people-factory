125d37983a08f0b461f842205c67ae126db3563f
Brandon Loi <brandon.loi@gmail.com>
2017-06-06 09:35:55 -0400

    Add a name to details

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ function HandleSubmit(ev) {
   const f = ev.target
   const name = f.personName.value
   const age = f.age.value
-  document.querySelector('h1').textContent = name + ' ' + age
+  const details = document.querySelector('#details')
+  //document.querySelector('h1').textContent = name + ' ' + age
   document.getElementById('blank').textContent += name + '-' + age + ' \n'
+  details.innerHTML = '<em>' + name + '</em>';
 }
 
 function fontSize(ev) {
