7e6de72088e7117fbf0d8d05ecb9294d2c6319c4
Brandon Loi <brandon.loi@gmail.com>
2017-06-06 11:41:09 -0400

    Rewrite Output in object oriented style

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
 const personForm = document.querySelector('form#personForm')
 const size = document.querySelector('form#size')
 
-
 function HandleSubmit(ev) {
   ev.preventDefault()
   const f = ev.target
-  const name = f.personName.value
   const details = document.querySelector('#details')
-  const favoriteColor = f.favoriteColor.value;
-  const age = f.age.value
-  const colorDiv = `
-  <div style="background-color: ${favoriteColor}; width: 100px; height:50px; "></div>`
-  //document.querySelector('h1').textContent = name + ' ' + age
-  //document.getElementById('blank').textContent += name + '-' + age + ' \n'
-  //details.innerHTML = `<em>${name}</em>`;
-  // const em = document.createElement('em')
-  // em.textContent = name;
-  // details.appendChild(em
-  // details.innerHTML = `
-  //   <ul>
-  //     <li>${name}</li>
-  //     <li>${colorDiv}</li>
-  //     <li>${age}</li>
-  //   </ul>
-  // `
-  const ul = document.createElement('ul')
-  const li1 = document.createElement('li')
-  li1.textContent = `Name: ${name}`
-  const li2 = document.createElement('li')
-  li2.textContent = `Color: ${favoriteColor}`
-  const li3 = document.createElement('li')
-  li3.textContent = `Age: ${age}`
 
-  details.appendChild(ul)
-  details.appendChild(li1)
-  details.appendChild(li2)
-  details.appendChild(li3)
+  const person = {
+    name: f.personName.value,
+    color: renderColor(f.favoriteColor.value).outerHTML,
+    age: f.age.value,
+  }
+
+  details.appendChild(renderList(person))
 }
 
 function fontSize(ev) {
   ev.preventDefault()
   document.getElementById('sizeChange').style.fontSize = "20px"
+}
 
+function renderListItem(fieldName, value) {
+  const li = document.createElement('li')
+  li.innerHTML = `${fieldName}: ${value}`
+  return li
+}
+function renderList(personData) {
+  const ul = document.createElement('ul')
+  //[name, favcolor, age]
+  Object.keys(personData).map(function(fieldName) {
+    const li = renderListItem(fieldName, personData[fieldName])
+    ul.appendChild(li)
+  })
+  return ul
+}
+
+function renderColor(color) {
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = color
+  colorDiv.style.width = '100px'
+  colorDiv.style.height = '50px'
+  return colorDiv
 }
 
 
